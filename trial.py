e27b5faa1b694e7c576b532df4db7ef0d90cec1c
Aderyn344 <aderynkusimbi@gmail.com>
2024-09-27 09:07:23 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.pipeline import Pipeline
