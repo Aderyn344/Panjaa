06dae855dc4152826a04ae08b7ca17ac19a479bd
Aderyn344 <aderynkusimbi@gmail.com>
2024-09-27 11:41:40 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-import joblib
+
 
 # Load your pre-trained model pipelines (replace with your actual loading logic)
 binary_pipeline = ...  # Load the binary classification pipeline
